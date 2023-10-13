024b9b93726bce86b3efd492b0d3a7d9555442ea
Opilane TTHK <opilane@tthk.ee>
2023-10-13 10:56:55 +0300

    As an app developer I want to get a list of all cars to show in my app [Delivers #186246657]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/cars', (req, res)=>{
+    res.send(["BMW", "M3"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
